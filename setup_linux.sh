ebf1c6706ac9640cf9053bbcee8137298d552edc
ma89vik <ma89vik@gmail.com>
2022-01-21 18:48:50 +0800

    build system: add base meson files

--- setup_linux.sh
+++ setup_linux.sh
@@ -4,7 +4,11 @@ export TOOLCHAIN_PATH="/home/marius/toolchains/gcc-arm-none-eabi-9-2020-q2-updat
 export NRF_SDK_PATH="/home/marius/projects/fang/ble_heart/external/nrf_sdk/"
 export NRFJPROG_PATH="nrfjprog"
 
+
 # Python setup (in a virtual env)
 python3 -m venv .ble_heart_venv
 source .ble_heart_venv/bin/activate
 pip3 install -r requirements.txt
+
+# add tools to path
+export PATH="$PATH:$TOOLCHAIN_PATH/bin"
